5e4c4560ad07db150a956abe3e70a586e16fa0bf
Lucas Augusto Hartmann <78509840+lucashartmann@users.noreply.github.com>
2026-04-06 21:31:22 -0300

    +-

--- view/config.py
+++ view/config.py
@@ -1,7 +1,7 @@
 from textual.screen import Screen
 from textual.widgets import Checkbox
 from textual.containers import Vertical, Grid, VerticalScroll, Horizontal, HorizontalGroup
-from textual.widgets import Button, Input, Select, Tabs, Tab, Footer
+from textual.widgets import Button, Input, Select, Tabs, Tab, Footer, MaskedInput
 from model.skills import diagrama, internet
 from textual_colorpicker import ColorPicker
 
@@ -13,6 +13,7 @@ class ConfigScreen(Screen):
     def on_screen_resume(self):
         self.query_one(Tabs).active = self.query_one(
             "#tab_config", Tab).id
+        # TODO: carregar dados do modelo
 
     def on_tabs_tab_activated(self, event: Tabs.TabActivated):
         try:
@@ -28,7 +29,11 @@ class ConfigScreen(Screen):
     def compose(self):
         yield Tabs(Tab("Chat", id="tab_chat"), Tab("Configuração", id="tab_config"), Tab("Skills", id="tab_skills"), Tab("Modelos", id="tab_models"))
         yield Input(placeholder="nome do bot", value="bot", id="input_nome_bot")
-        yield Input(placeholder="seu nome")
+        yield Input(placeholder="seu nome", id="input_nome_user")
+        yield MaskedInput(placeholder="temperatura", mask="0.0", id="input_temperatura")
+        yield MaskedInput(placeholder="máximo de tokens", mask="0000", id="input_max_tokens")
+        yield MaskedInput(placeholder="top_p", mask="0.0", id="input_top_p")
+        
         yield ColorPicker()
         
 
